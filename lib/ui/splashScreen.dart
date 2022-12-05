b59003362b295a9e0186f2ba019ba415d1391958
Imrh4u <imrh4u@gmail.com>
2022-12-05 18:28:32 +0530

    first commit

---
+++ lib/ui/splashScreen.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: initScreen(context),
+    );
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 6);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    // Navigator.pushReplacement(
+    //     context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
+  initScreen(BuildContext context) {
+    return SafeArea(
+        child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage("assets/images/arya_logo.png"),
+            height: 100.0,
+            width: 200.0,
+            alignment: AlignmentDirectional.center,
+          ),
+          SizedBox(height: 120),
+          Image(
+            image: AssetImage("assets/images/car.png"),
+            height: 150.0,
+            width: 250.0,
+            alignment: AlignmentDirectional.center,
+          ),
+          SizedBox(height: 120),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Text(
+                "Brand Of ",
+                style: TextStyle(color: Colors.black26, fontSize: 22.0),
+              ),
+              Text(
+                "Bihar",
+                style: TextStyle(color: Colors.black, fontSize: 22.0),
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Padding(padding: EdgeInsets.only(top: 20.0)),
+          // ignore: prefer_const_constructors
+          CircularProgressIndicator(
+            backgroundColor: Colors.black,
+            strokeWidth: 1,
+          )
+        ],
+      ),
+    ));
+  }
+}
---
+++ lib/utils/route.dart
@@ -0,0 +1,9 @@
+import 'package:aryago/ui/splashScreen.dart';
+import 'package:get/get.dart';
+
+class MyRoutes {
+  static var routes = [
+    GetPage(name: "/", page: () => SplashScreen(),)
+    // GetPage(name: "/", page: () => SplashScreen(),)
+  ];
+}
